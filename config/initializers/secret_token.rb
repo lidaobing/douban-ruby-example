df0134ae3639509e29fdcb72b2df197a5ce19b70
LI Daobing <lidaobing@gmail.com>
2011-01-02 16:50:09 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoubanRubyExample::Application.config.secret_token = '8103a24f2c8db782139e355cebba7f832b1109ac9b116406d31414602c213a7d2e3ef0177b3ef2ee5f0b3246af28f4d79d274c4f99034be163b5bf82be31ee99'
