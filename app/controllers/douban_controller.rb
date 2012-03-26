5a9c0ced33d8f12cfa0e8528b56dc9c41d11c26d
LI Daobing <lidaobing@gmail.com>
2012-03-26 19:04:46 +0800

    fix #2, support custom app key and secret

--- app/controllers/douban_controller.rb
+++ app/controllers/douban_controller.rb
@@ -5,11 +5,19 @@ class DoubanController < ApplicationController
   DOUBAN_SECRET = 'c59e3be2ccdde999'
 
   def index
+    reset_session
+    @douban_apikey = DOUBAN_APIKEY
+    @douban_secret = DOUBAN_SECRET
+  end
+
+  def authorize
     logger.info session.inspect
     unless params[:oauth_token]
       unless session[:access_token]
+        session[:douban_apikey] = params[:apikey] || DOUBAN_APIKEY
+        session[:douban_secret] = params[:secret] || DOUBAN_SECRET
         # step 1, initial state, store request_token to session
-        callback_url = url_for :action => :index
+        callback_url = url_for :action => :authorize
         redirect_url = douban.get_authorize_url(callback_url)
         session[:request_token] = douban.request_token :as_hash
         redirect_to redirect_url
@@ -18,7 +26,7 @@ class DoubanController < ApplicationController
         douban.access_token = session[:access_token]
         @access_token = douban.access_token
         @people = douban.get_people
-        render :index, :content_type => Mime::TEXT, :layout => false
+        render :authorize, :content_type => Mime::TEXT, :layout => false
         #render :text => douban.get_people.inspect, :content_type => Mime::TEXT
       end
     else
@@ -26,19 +34,23 @@ class DoubanController < ApplicationController
         # step 2, return from douban, store access_token to session
         douban.request_token = session[:request_token]
         douban.auth
+        douban_apikey = session[:douban_apikey]
+        douban_secret = session[:douban_secret]
         reset_session
         session[:access_token] = douban.access_token :as_hash
-        redirect_to :action => :index
+        session[:douban_apikey] = douban_apikey
+        session[:douban_secret] = douban_secret
+        redirect_to :action => :authorize
       else
         # error branch, you return from douban, but request_token
         logger.info "return from oauth but no request_token"
-        redirect_to :action => :index
+        redirect_to :action => :authorize
       end
     end
   end
 
   private
   def douban
-    @douban ||= Douban::Authorize.new DOUBAN_APIKEY, DOUBAN_SECRET
+    @douban ||= Douban::Authorize.new session[:douban_apikey], session[:douban_secret]
   end
 end
